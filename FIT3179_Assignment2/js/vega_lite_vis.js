84b5ac156e1cd71e00853b0ed76e8cdc971c48dc
liawyihui <88360940+liawyihui@users.noreply.github.com>
2023-09-17 01:48:37 +0800

    Update FIT3179_Assignment2/js/vega_lite_vis.js

--- FIT3179_Assignment2/js/vega_lite_vis.js
+++ FIT3179_Assignment2/js/vega_lite_vis.js
@@ -1,4 +1,5 @@
-var world_map = "js/average_price_world_amp.vg.json";
+var world_map = "js/average_price_world_map
+.vg.json";
 vegaEmbed('#world_map', world_map).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
