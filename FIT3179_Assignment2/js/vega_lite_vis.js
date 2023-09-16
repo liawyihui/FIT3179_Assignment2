9dd7ebc7fa8219af7494ad28879be96cc736b4da
liawyihui <88360940+liawyihui@users.noreply.github.com>
2023-09-17 03:03:09 +0800

    Deleting FIT3179_Assignment2

--- FIT3179_Assignment2/js/vega_lite_vis.js
+++
@@ -1,9 +0,0 @@
-var world_map = "js/average_price_world_map.vg.json";
-vegaEmbed('#world_map', world_map).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-
-var country_distribution_bar_chart = "js/country_distribution_bar_chart.vg.json";
-vegaEmbed('#country_distribution_bar_chart', country_distribution_bar_chart).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
