ac51e5c6f5696c1a231de86d6302091f73c6f72d
liawyihui <ylia0072@student.monash.edu>
2023-10-06 21:13:52 +0800

    rename

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -3,8 +3,8 @@ vegaEmbed('#world_map', world_map, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var country_distribution_bar_chart = "js/country_distribution_bar_chart.vg.json";
-vegaEmbed('#country_distribution_bar_chart', country_distribution_bar_chart, { "actions": false }).then(function(result) {
+var property_distribution_bar_chart = "js/property_distribution_bar_chart.vg.json";
+vegaEmbed('#property_distribution_bar_chart', country_distribution_bar_chart, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
