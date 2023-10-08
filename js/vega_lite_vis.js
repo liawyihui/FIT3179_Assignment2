8b6e7b905c889739517f6255a9a820adfbae2ff8
liawyihui <ylia0072@student.monash.edu>
2023-10-09 00:07:05 +0800

    added chart into html

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -17,3 +17,8 @@ var bubble_chart = "js/bubble_plot_area_rooms_price.vg.json";
 vegaEmbed('#bubble_chart', bubble_chart, { "actions": false }).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var parallel_connections = "js/relationships_between_details.vg.json";
+vegaEmbed('#parallel_connections', parallel_connections, { "actions": false }).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
